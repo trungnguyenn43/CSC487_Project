4bf26ce49f84ffce0f331db08e161c325923e2f4
2atm <125700363+trungnguyenn43@users.noreply.github.com>
2025-11-06 16:07:56 -0600

    Refactor: Update certificate generation to include modulus n and improve menu structure

--- Proj3AB_GenSignVerifyCerts/driver.c
+++ Proj3AB_GenSignVerifyCerts/driver.c
@@ -10,6 +10,7 @@ int main() {
     unsigned int p, q;
     unsigned int e = 0;
     unsigned int d = 0;
+    unsigned int n = 0;
 
     do{
 
@@ -20,6 +21,7 @@ int main() {
 
         if(isPrime(p) && isPrime(q)){
             d = RSA_KeyGen(p, q, &e);
+            n = p * q;
             printf("Public key (e): %u\n", e);
             printf("Private key (d): %u\n", d);
             printf("========================================\n\n");
@@ -37,7 +39,33 @@ int main() {
         
     }while(1);
     
-    certGen(d, e); 
+    do{
+        // create menu
+        char inputBuffer[10];
+        int choice = 0;
+        printf("Please select an option:\n");
+        printf("1. Generate Certificate\n");
+        printf("2. Verify Certificate\n");
+        printf("3. Exit\n");
+        printf("Enter choice >> ");
+        fgets(inputBuffer, sizeof(inputBuffer), stdin);
+        choice = atoi(inputBuffer);
+
+        switch(choice){
+            case 1:
+                certGen(d, e, n); // pass private, public key, and n
+                break;
+            case 2:
+                certVerify();
+                break;
+            case 3:
+                printf("Exiting program.\n");
+                return 0;
+            default:
+                break;
+        }
+    }while(1);
+    
 
     return 0;
 }
